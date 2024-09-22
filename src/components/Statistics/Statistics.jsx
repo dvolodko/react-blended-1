55078c40db63b73f98d9aa9c8a8916812e7af2fe
Bohdan Haponiuk <jlomka54@gmail.com>
2024-09-22 20:34:17 +0400

    add task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,32 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import { StatisticsItem } from '../StatisticsItem/StatisticsItem';
+import style from './Statistics.module.css';
+const icons = [
+  FaRegThumbsUp,
+  MdPeople,
+  MdOutlineProductionQuantityLimits,
+  GiTreeDoor,
+];
+export const Statistics = ({ title, stats }) => {
+  return (
+    <>
+      {title && <h3 className={style.title}>{title}</h3>}
 
-export const Statistics = () => {
-  return <h2>Statistics</h2>;
+      <ul className={style.list}>
+        {stats.map((stat, index) => {
+          return (
+            <li key={stat.id} className={style.item}>
+              <StatisticsItem
+                title={stat.title}
+                total={stat.total}
+                icon={icons[index]}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
